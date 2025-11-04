594b7c50701d46301a5e7f4c2a9ad4e1713a4f4f
Raja 👽👾🤖 <126279774+Rabenarivo@users.noreply.github.com>
2025-11-04 10:25:46 +0300

    bis 2

--- framework/annotation/UrlMapping.java
+++ framework/annotation/UrlMapping.java
@@ -1,6 +1,5 @@
 package framework.annotation;
 
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
--- framework/annotation/UserController.java
+++ framework/annotation/UserController.java
@@ -2,10 +2,14 @@ package framework.annotation;
 
 @UrlMapping(url = "https://example.com/api/users")
 public class UserController {
-    
+
     @UrlMapping(url = "/list")
     public void listUsers() {
         System.out.println("Liste des utilisateurs");
     }
-}
 
+    @UrlMapping(url = "/add")
+    public void addUser() {
+        System.out.println("Ajout d’un utilisateur");
+    }
+}
